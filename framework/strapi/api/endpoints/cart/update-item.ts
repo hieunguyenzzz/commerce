16cf6c57678fb1c0582c218141dc981c6624b306
ngohoanglong <ngohoanglongptit@gmail.com>
2021-09-09 09:07:25 +0700

    remove unused

--- framework/strapi/api/endpoints/cart/update-item.ts
+++ framework/strapi/api/endpoints/cart/update-item.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { NEXT_PUBLIC_STRAPI_URL } from '@framework/const'
 import { UpdateQuoteItemMutation } from '@framework/schema'
 import { Cart } from '@framework/types/cart'
@@ -61,48 +60,7 @@ const updateQuoteItemMutation = /* GraphQl */`mutation updateQuoteItem($id: ID!,
     }
   }
 }`
-const nomalize = (res:UpdateQuoteItemMutation): Cart =>{
-  console.log(res)
-  const quote =  res.updateQuoteItem?.quoteItem?.quote
-  const cart:any =  {
-    id:''+ quote?.id,
-    customerId: '0',
-    email: 'no.noo.nooo.yes@gmail.com',
-    currency: { code: 'USD' },
-    taxesIncluded: quote?.taxesIncluded||false,
-    lineItems:[],
-    lineItemsSubtotalPrice: 0,
-    subtotalPrice: 0,
-    totalPrice: 0,
-  }
-  cart.lineItems = (quote?.lineItems||[]).map(quoteItem=>({
-    id: quoteItem?.id+'',
-    variantId: quoteItem?.variantId+'',
-    productId: quoteItem?.productId?.id+'',
-    name: quoteItem?.productId?.title+'',
-    quantity: quoteItem?.quantity||1,
-    variant: {
-      id:  quoteItem?.productId?.id+'',
-      name: quoteItem?.productId?.title+'',
-      image: {
-        url: NEXT_PUBLIC_STRAPI_URL+ quoteItem?.productId?.images?.[0]?.url+'',
-      },
-      requiresShipping: true,
-      price: quoteItem?.productId?.price||0,
-      listPrice: quoteItem?.productId?.price||0,
-    },
-    path:  quoteItem?.productId?.slug+'',
-    discounts: [],
-  }))
-  cart.totalPrice = cart.lineItems.reduce((result=0,value:any)=>{
-    console.log( result,value.quantity*value.variant.price, result+value.quantity*value.variant.price)
-    return result+Number(value.quantity*value.variant.price)
-  },0)
-  cart.subtotalPrice=cart.totalPrice
-  cart.lineItemsSubtotalPrice=cart.totalPrice
-  console.log({cart})
-  return cart
-}
+
 const updateItem: CartEndpoint['handlers']['updateItem'] = async ({
   res,
   req,
@@ -112,22 +70,12 @@ const updateItem: CartEndpoint['handlers']['updateItem'] = async ({
   let result: { data?: any } = {}
   const { cookies } = req
   const cartId = cookies[config.cartCookie]
-=======
-import type { CartEndpoint } from '.'
-
-const updateItem: CartEndpoint['handlers']['updateItem'] = async ({
-  res,
-  body: { cartId, itemId, item },
-  config,
-}) => {
->>>>>>> 1f5c670e9483a0337d9868cd78aefdd8b4861edd
   if (!cartId || !itemId || !item) {
     return res.status(400).json({
       data: null,
       errors: [{ message: 'Invalid request' }],
     })
   }
-<<<<<<< HEAD
   if (cartId) {
     try {
       console.log({item,itemId})
@@ -144,25 +92,6 @@ const updateItem: CartEndpoint['handlers']['updateItem'] = async ({
     }
   }
   res.status(200).json({ data:nomalizeCart(result.data?.updateQuoteItem?.quoteItem?.quote) })
-=======
-
-  // const { data } = await config.storeApiFetch(
-  //   `/v3/carts/${cartId}/items/${itemId}?include=line_items.physical_items.options`,
-  //   {
-  //     method: 'PUT',
-  //     body: JSON.stringify({
-  //       line_item: parseCartItem(item),
-  //     }),
-  //   }
-  // )
-
-  // // Update the cart cookie
-  // res.setHeader(
-  //   'Set-Cookie',
-  //   getCartCookie(config.cartCookie, cartId, config.cartCookieMaxAge)
-  // )
-  // res.status(200).json({ data: normalizeCart(data) })
->>>>>>> 1f5c670e9483a0337d9868cd78aefdd8b4861edd
 }
 
 export default updateItem
