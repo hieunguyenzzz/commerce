d141596c1b9cbc4b4a0b4cf95f3bcb94e977145f
ngohoanglong <ngohoanglongptit@gmail.com>
2021-09-13 17:51:51 +0700

    fix build error

--- framework/strapi/api/endpoints/logout/logout.ts
+++ framework/strapi/api/endpoints/logout/logout.ts
@@ -1,13 +1,10 @@
 import { STRAPI_JWT } from '@framework/const'
 import { serialize } from 'cookie'
 import type { LogoutEndpoint } from '.'
-import { getCartCookie } from '../cart/get-cart'
 
 const logout: LogoutEndpoint['handlers']['logout'] = async ({ res }) => {
   // Remove the cookie
   res.setHeader('Set-Cookie',[ serialize(STRAPI_JWT, '', { maxAge: -1,path: '/' }),serialize('cartCookie', '', { maxAge: -1,path: '/' })])
-
-
   // Only allow redirects to a relative URL
   res.status(200).json({ data: null })
 }
